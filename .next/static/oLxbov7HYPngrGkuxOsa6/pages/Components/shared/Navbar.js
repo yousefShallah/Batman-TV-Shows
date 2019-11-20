50b4718d055c6a3a69c67d1745e0898227856a20
Yousef <yousefmohshallah33@gmail.com>
2019-11-20 13:02:16 +0200

    build project

---
+++ .next/static/oLxbov7HYPngrGkuxOsa6/pages/Components/shared/Navbar.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"81IV":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Components/shared/Navbar",function(){return w("B3Nz")}])}},[["81IV",0,1]]]);
